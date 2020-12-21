33eb6c71bd5b4cacb137160d530b31ede09d6da9
S.F <1074260090@qq.com>
2020-12-21 18:04:14 +0800

    first commit

---
+++ assets/js/index.ts
@@ -0,0 +1,28 @@
+// Learn TypeScript:
+//  - https://docs.cocos.com/creator/manual/en/scripting/typescript.html
+// Learn Attribute:
+//  - https://docs.cocos.com/creator/manual/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - https://docs.cocos.com/creator/manual/en/scripting/life-cycle-callbacks.html
+
+import { _decorator, Component, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('Typescript')
+export class Typescript extends Component {
+    /* class member could be defined like this */
+    // dummy = '';
+
+    /* use `property` decorator if your want the member to be serializable */
+    // @property
+    // serializableDummy = 0;
+
+    start () {
+        console.log(1);
+        // Your initialization goes here.
+    }
+
+    // update (deltaTime: number) {
+    //     // Your update function goes here.
+    // }
+}
---
+++ assets/js/player.ts
@@ -0,0 +1,32 @@
+// Learn TypeScript:
+//  - https://docs.cocos.com/creator/manual/en/scripting/typescript.html
+// Learn Attribute:
+//  - https://docs.cocos.com/creator/manual/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - https://docs.cocos.com/creator/manual/en/scripting/life-cycle-callbacks.html
+
+import { _decorator, Component, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('Player')
+export class Player extends Component {
+    /* class member could be defined like this */
+    // dummy = '';
+    
+    /* use `property` decorator if your want the member to be serializable */
+    // 生命值
+    @property
+    HP = 0;
+
+    // 攻击力
+    @property
+    ATK = 0;
+
+    start () {
+        // Your initialization goes here.
+    }
+
+    // update (deltaTime: number) {
+    //     // Your update function goes here.
+    // }
+}
