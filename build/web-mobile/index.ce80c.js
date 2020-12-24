ce44c18e592b29eac059474f710027d2a1fbb11a
S.F <1074260090@qq.com>
2020-12-24 15:19:18 +0800

    save

--- build/web-mobile/index.ce80c.js
+++ build/web-mobile/index.ce80c.js
@@ -1,4 +1,4 @@
-System.register(["./application.js"], function (_export, _context) {
+System.register(["./application.34aac.js"], function (_export, _context) {
   "use strict";
 
   var createApplication, canvas, $p, bcr;
@@ -85,8 +85,8 @@ System.register(["./application.js"], function (_export, _context) {
   }
 
   return {
-    setters: [function (_applicationJs) {
-      createApplication = _applicationJs.createApplication;
+    setters: [function (_application34aacJs) {
+      createApplication = _application34aacJs.createApplication;
     }],
     execute: function () {
       canvas = document.getElementById('GameCanvas');
