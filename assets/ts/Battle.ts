53738715d69b63073442860e0ced1c3428ce02e0
S.F <1074260090@qq.com>
2020-12-28 17:36:41 +0800

    1

--- assets/ts/Battle.ts
+++ assets/ts/Battle.ts
@@ -5,91 +5,93 @@
 // Learn life-cycle callbacks:
 //  - https://docs.cocos.com/creator/manual/en/scripting/life-cycle-callbacks.html
 
-import { _decorator, Component, Node, Label, Button, find, Canvas, UIOpacity, UITransform } from 'cc';
+import { _decorator, Component, Button, find, Prefab, instantiate, Label } from 'cc';
 const { ccclass, property, integer, float, boolean, string, type } = _decorator;
 import { Player } from "./Player";
 import { Enemy } from "./Enemy";
 
 @ccclass('Battle')
 export class Battle extends Component {
-    callback: () => void;
-    startBtn: Node;
-    enemyHp = 10;
-    enemyATK = 1.5;
-    battleShow: boolean = true;
+    @property({ type: Prefab })
+    private enemyPre: Prefab = null;
 
+    enemyData: any;
+    battling: () => void;
+    playerData: any;
+    enemyNodes: any[] = [];
+    //怪物生成数量
+    num: number = 10;
+    result: string;
+
+    onLoad() {
+        //加载玩家数据
+        this.playerData = find("Canvas/Player").getComponent(Player).playerData;
+        //加载敌人数据
+        this.getenemyData()
+    }
     start() {
-        this.Battle()
+        find("Canvas/Battle/Button").on(Button.EventType.CLICK, this.btnClick, this)
+        //生成敌人(数量)
+        // this.generateEnemies(10)
+
     }
     update(deltaTime: number) {
-        //fight
-        let PlayerCom = find("Canvas/Player").getComponent(Player).playerData
-        let EnemyCom = find("Canvas/Battle/Enemy").getComponent(Enemy)
-        find("Canvas/Battle/Player/Info").getComponent(Label).string = "Player\nHP：" + PlayerCom.HP.toString()
-        find("Canvas/Battle/Enemy/Info").getComponent(Label).string = "Enemy\nHP：" + EnemyCom.HP.toString() + "\nATK: " + EnemyCom.ATK.toString()
-
-        find("Canvas/Battle/ScrollView/view/content").getComponent(UITransform).height = find("Canvas/Battle/ScrollView/view/content/item").getComponent(UITransform).height
+        //判断结果
+        if (this.playerData.HP <= 0){
+            this.playerData.HP = 0;
+            this.result = 'fail'
+            console.log('失败了')
+        }
     }
-    Battle() {
-        this.startBtn = find("Canvas/Battle/Button")
-        this.startBtn.on(Button.EventType.CLICK, this.btnClick, this)
+    generateEnemies(num){
+        for (let i = 0; i < num; i++) {
+            let node = instantiate(this.enemyPre);
+            node.parent = find("Canvas/Battle/Enemies");
+        }
     }
     btnClick(button: Button) {
-        // 注意这种方式注册的事件，无法传递 customEventData
-        let player = find("Canvas/Player").getComponent(Player)
-        let enemy = find("Canvas/Battle/Enemy").getComponent(Enemy)
-        this.startBtn.getComponent(Button).interactable = false
-        this.checkNums()
-    }
-    checkNums() {
-        //战斗结果
-        let result = find("Canvas/Battle/ScrollView/view/content/item").getComponent(Label)
-        result.string = '\n' + result.string
-        let player = find("Canvas/Player").getComponent(Player).playerData
-        let enemy = find("Canvas/Battle/Enemy").getComponent(Enemy)
-        this.schedule(this.callback = function () {
-            let damgeReult = player.ATK
-            if (Math.random() < player.Crit) {
-                //暴击成功
-                damgeReult = player.ATK*player.CritD
-                result.string = '暴击了！你造成了' + damgeReult + '点伤害，受到了' + enemy.ATK + '点伤害\n' + result.string
-            } else {
-                result.string = '你造成了' + damgeReult + '点伤害，受到了' + enemy.ATK + '点伤害\n' + result.string
-            }
-
-            // 怪物血量
-            enemy.HP = Number((enemy.HP - damgeReult).toFixed(2))
-
-            // 玩家血量
-            player.HP -= enemy.ATK
-
-            let battleResult
-            //player失败
-            if (player.HP <= 0) {
-                player.HP = 0
-                battleResult = '战斗失败\n' + result.string
-            }
-
-            //enemy失败
-            if (enemy.HP <= 0) {
-                let enemyExp = 3
-                player.EXP += enemyExp;
-                // find("Canvas/Player").getComponent(Player).checkLevel()
-                this.enemyHp += 4
-                this.enemyATK += 1
-                enemy.HP = 0
-                battleResult = '战斗成功，获得' + enemyExp+'点经验值\n' + result.string
-            }
-            //结束
-            if (player.HP <= 0 || enemy.HP <= 0) {
-                result.string = battleResult
-                find("Canvas/Player").getComponent(Player).setPlayerData(player)
-                find("Canvas/Player").getComponent(Player).reset(player.MaxHp);
-                enemy.reset(this.enemyHp, this.enemyATK);
-                this.startBtn.getComponent(Button).interactable = true;
-                this.unschedule(this.callback);
+        let num = this.num
+        for (let i = 0; i < num; i++) {
+            let node = instantiate(this.enemyPre);
+            node.parent = find("Canvas/Battle/Enemies");
+            this.enemyNodes.push(node)
+        }
+        this.battleProcess();
+        this.schedule(this.battling = function () {
+            for (let i = 0; i < num; i++) {
+                let enemyNow = this.enemyNodes[i].getComponent(Enemy).enemyNow
+                this.playerData.HP = Number((this.playerData.HP - enemyNow.ATK).toFixed())
             }
         }, 1);
     }
+    battleProcess(){
 
+    }
+    getenemyData() {
+        this.enemyData = JSON.parse(localStorage.getItem('enemyData'));
+        //如果不存在怪物数据则新建
+        if (!this.enemyData) {
+            this.enemyData = {
+                white: {
+                    Level: 'white',
+                    MaxHp: '200',
+                    ATK: '2',
+                },
+                blue: {
+                    Level: 'blue',
+                    MaxHp: '300',
+                    ATK: '3',
+                    chance: 0.3,//生成几率,实际为0.2 (0.3-0.1
+                },
+                gold: {
+                    Level: 'gold',
+                    MaxHp: '500',
+                    ATK: '5',
+                    chance: 0.1,//生成几率
+                }
+            }
+            localStorage.setItem('enemyData', JSON.stringify(this.enemyData))
+        }
+        console.log(this.enemyData);
+    }
 }
--- assets/ts/Enemy.ts
+++ assets/ts/Enemy.ts
@@ -5,31 +5,32 @@
 // Learn life-cycle callbacks:
 //  - https://docs.cocos.com/creator/manual/en/scripting/life-cycle-callbacks.html
 
-import { _decorator, Component, Node } from 'cc';
+import { _decorator, Component, Node, find, Label } from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('Enemy')
 export class Enemy extends Component {
-    /* class member could be defined like this */
-    // dummy = '';
-
-    /* use `property` decorator if your want the member to be serializable */
-    // 生命值
-    @property
-    HP = 0;
-
-    // 攻击力
-    @property
-    ATK = 0;
-
-    start () {
-        // Your initialization goes here.
+    enemyData: any;
+    enemyNow: any;
+    onLoad () {
+        //随机出怪物属性
+        this.giveEnemyProperty()
     }
-    reset(HP,ATK) {
-        this.HP = HP;
-        this.ATK = ATK;
+    update(deltaTime: number) {
     }
-    update (deltaTime: number) {
-        // Your update function goes here.
+    giveEnemyProperty(){
+        this.enemyData = JSON.parse(localStorage.getItem('enemyData'));
+        let enemyData = this.enemyData
+        //当前生成的怪物属性
+        let enemyNow 
+        if (Math.random() < enemyData.gold.chance) {
+            enemyNow = enemyData.gold
+        } else if (Math.random() < enemyData.blue.chance && Math.random() > enemyData.gold.chance){
+            enemyNow = enemyData.blue
+        } else {
+            enemyNow = enemyData.white
+        }
+        this.enemyNow = enemyNow
+        find("Info", this.node).getComponent(Label).string = enemyNow.Level + '\n血量：' + enemyNow.MaxHp + '\n攻击力：' + enemyNow.ATK 
     }
 }
